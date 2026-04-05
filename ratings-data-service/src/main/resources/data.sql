97714f8859220f857d6411a5f71e45fcf3aba296
OmniaTarek18 <love2828day@gmail.com>
2026-04-05 17:16:26 +0200

    updata sql.data to generate synthetic data

--- ratings-data-service/src/main/resources/data.sql
+++ ratings-data-service/src/main/resources/data.sql
@@ -1,5 +1,11 @@
-INSERT INTO ratings (user_id, movie_id, rating) VALUES
-('user1', '550', 4),
-('user1', '551', 3),
-('user2', '550', 5),
-('user2', '552', 2);
+INSERT IGNORE INTO ratings (user_id, movie_id, rating)
+WITH RECURSIVE seq AS (
+    SELECT 1 AS n
+    UNION ALL
+    SELECT n + 1 FROM seq WHERE n < 1000
+)
+SELECT 
+    CONCAT('user_', FLOOR(1 + RAND() * 500)),  -- Random user ID between 1-500
+    CONCAT('movie_', FLOOR(1 + RAND() * 50)), -- Random movie ID between 1-50
+    FLOOR(1 + RAND() * 5)                      -- Random rating between 1-5
+FROM seq;
