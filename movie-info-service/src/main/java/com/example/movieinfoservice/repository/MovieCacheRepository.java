c1320e156748be457b0e024eb0838271613c8a81
Rana Mohamed <121067602+RanaMohamed6720@users.noreply.github.com>
2026-04-05 05:08:16 +0000

    feat: add MovieCacheRepository

---
+++ movie-info-service/src/main/java/com/example/movieinfoservice/repository/MovieCacheRepository.java
@@ -0,0 +1,20 @@
+package com.example.movieinfoservice.repository;
+
+import com.example.movieinfoservice.entity.MovieCacheEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Repository
+public interface MovieCacheRepository extends MongoRepository<MovieCacheEntity, String> {
+    Optional<MovieCacheEntity> findByMovieId(String movieId);
+    
+    // find a non expired cached movie by movieID
+    @Query("{ 'movieId': ?0, 'ttl': { $gt: ?1 } }")
+    Optional<MovieCacheEntity> findValidCacheByMovieId(String movieId, LocalDateTime now);
+
+    void deleteByMovieId(String movieId);
+}
